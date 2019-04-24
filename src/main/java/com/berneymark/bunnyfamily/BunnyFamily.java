a4a216c5234b32e0187880aa79b2dd000cea18ac
Mark Berney <berneydwmark@gmail.com>
2019-04-25 09:44:57 +1200

    events and first class

--- src/main/java/com/berneymark/bunnyfamily/BunnyFamily.java
+++ src/main/java/com/berneymark/bunnyfamily/BunnyFamily.java
@@ -1,13 +1,33 @@
 package com.berneymark.bunnyfamily;
 
+import com.berneymark.bunnyfamily.events.OnDeath;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerShearEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BunnyFamily extends JavaPlugin {
+public final class BunnyFamily extends JavaPlugin implements Listener {
+    @EventHandler
+    public void onLeaveBed(PlayerBedLeaveEvent e) {
+        Player player = e.getPlayer();
+        player.sendMessage("Sweet dreams!");
+    }
+
+    @EventHandler
+    public void onShearSheep(PlayerShearEntityEvent e) {
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.GREEN + "Nice try!");
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Running...");
+        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new OnDeath(), this);
     }
 
     @Override
---
+++ src/main/java/com/berneymark/bunnyfamily/events/OnDeath.java
@@ -0,0 +1,14 @@
+package com.berneymark.bunnyfamily.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class OnDeath implements Listener {
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        e.getEntity().sendMessage("RIP my friend.");
+        e.getEntity().setFlying(true);
+    }
+}
